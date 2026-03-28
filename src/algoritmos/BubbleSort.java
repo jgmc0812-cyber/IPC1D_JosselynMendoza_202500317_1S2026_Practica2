e574670407e555d82943f4b7047928089bd288a6
lopas <lopas@Mendoza>
2026-03-28 01:37:55 -0600

    Realización de la clase "BubbleSort"

--- src/algoritmos/BubbleSort.java
+++ src/algoritmos/BubbleSort.java
@@ -1,6 +1,35 @@
 //ORDENAMIENTO DE BURBUJA: compara elementos vecinos y los intercambia si están en el orden incorrecto
 package algoritmos;
 
+import modelo.DatosOrdenamiento;
+
 public class BubbleSort {
     
+    public void sort(DatosOrdenamiento datos){
+        int[]arreglo = datos.getArreglo();
+        int n = arreglo.length;
+        
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n - 1 - i; j++){
+                if(datos.getOrden().equals("Ascendente")){
+                    if(arreglo[j] > arreglo[j+1]){
+                        int temp = arreglo[j];
+                        arreglo[j] = arreglo[j+1];
+                        arreglo[j+1] = temp;
+                        datos.setIntercambios(datos.getIntercambios() + 1);
+                    }
+                } else{
+                    if(arreglo[j] < arreglo[j+1]){
+                        int temp = arreglo[j];
+                        arreglo[j] = arreglo[j+1];
+                        arreglo[j+1] = temp;
+                        datos.setIntercambios(datos.getIntercambios() + 1);
+                    }
+                }
+                datos.setComparaciones(datos.getComparaciones() + 1);
+            }
+            datos.setIteraciones(datos.getIteraciones() + 1);
+        
+        }
+    }   
 }
--- src/modelo/DatosOrdenamiento.java
+++ src/modelo/DatosOrdenamiento.java
@@ -4,7 +4,100 @@ package modelo;
 //TODOS LOS DATOS SE ALMACENARÁN AQUÍ. Guarda todos los datos que las demás clases necesitan compartir.
 
 public class DatosOrdenamiento {
+    // Atributos
+    private int[] arreglo;
+    private int[] arregloOriginal;
+    private int comparaciones;
+    private int intercambios;
+    private int iteraciones;
+    private String algoritmo;
+    private String orden;
+    private int velocidad;
     
+    //Constructor
+    public DatosOrdenamiento(){
+        comparaciones = 0;
+        intercambios = 0;
+        iteraciones = 0;
+        algoritmo = "Bubble Sort";
+        orden = "Ascendente";
+        velocidad = 0;
+    }
     
+    //get y set del arreglo
+    public int[] getArreglo(){
+        return arreglo;
+    }
+    
+    public void setArreglo (int[] arreglo){
+        this.arreglo = arreglo;
+        this.arregloOriginal = arreglo.clone();
+    }
+    
+    //solo lleva get porque no queremos darle ningún valor
+    public int[] getArregloOriginal (){
+        return arregloOriginal;
+    }
+    
+    //get y set de comparaciones
+    public int getComparaciones(){
+        return comparaciones;
+    }
+    
+    public void setComparaciones(int comparaciones){
+        this.comparaciones = comparaciones;
+    }
+    
+    //get y set de intercambios
+    public int getIntercambios(){
+        return intercambios;
+    }
+    
+    public void setIntercambios(int intercambios){
+        this.intercambios = intercambios;
+    }
+    
+    //get y set de iteraciones
+    public int getIteraciones(){
+        return iteraciones;
+    }
+    
+    public void setIteraciones(int iteraciones){
+        this.iteraciones = iteraciones;
+    }
+    
+    //get y set de algoritmo
+    public String getAlgoritmo(){
+        return algoritmo;
+    }
+    
+    public void setAlgortimo(String algoritmo){
+        this.algoritmo = algoritmo;
+    }
+    
+    //get y set de orden
+    public String getOrden(){
+        return orden;
+    }
+    
+    public void setOrden(String orden){
+        this.orden = orden;
+    }
+    
+    //get y set de velocidad
+    public int getVelocidad(){
+        return velocidad;
+    }
+    
+    public void setVelocidad(int velocidad){
+        this.velocidad = velocidad;
+    }
+    
+    //reinicia los contadores
+    public void reiniciarEstadisticas(){
+        comparaciones = 0;
+        iteraciones = 0;
+        intercambios = 0;
+    }
     
 }
